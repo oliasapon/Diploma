9aa5412c48d48de44cbb9eabad390315a4d59a22
oliasapon <50492072+oliasapon@users.noreply.github.com>
2021-09-21 18:48:58 +0300

    Update ConnectionSettings.java

--- src/test/java/pageSettings/ConnectionSettings.java
+++ src/test/java/pageSettings/ConnectionSettings.java
@@ -13,7 +13,7 @@ import java.net.URL;
 public class ConnectionSettings {
     public static AndroidDriver<AndroidElement> androidDriver;
     @BeforeClass
-    public void connection/*init*/() throws MalformedURLException, InterruptedException {
+    public void connection() throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
@@ -30,39 +30,3 @@ public class ConnectionSettings {
     public void close(){
         androidDriver.quit();
     }
-
-
-    /*public static AppiumDriver<MobileElement> androidDriver;
-    @BeforeClass
-    public void init() throws MalformedURLException, InterruptedException {
-
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("appPackage", "com.cardiolyse");
-        desiredCapabilities.setCapability("appActivity", ".MainActivity");
-
-        androidDriver = new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-        androidDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        Thread.sleep(10000, 30);
-    }*/
-
-
-
-    /*public static AppiumDriver<MobileElement> driver;
-    @BeforeTest
-    @Parameters({"deviceName","udid", "platformVersion","url"})
-    public void setup(String deviceName, String udid,String platformVersion,String url) throws InterruptedException, MalformedURLException
-    {
-        DesiredCapabilities cap=new DesiredCapabilities();
-        cap.setCapability("deviceName", deviceName);
-        cap.setCapability("udid", udid);
-        cap.setCapability("appActivity", "com.android.calculator2.Calculator");
-        cap.setCapability("appPackage", "com.android.calculator2");
-        cap.setCapability("platformName", "Android");
-        cap.setCapability("platformVersion", platformVersion);
-        driver=new AppiumDriver<MobileElement>(new URL(url), cap);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        Thread.sleep(5000);*/
-}
